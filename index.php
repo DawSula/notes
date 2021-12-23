eeade96a226900358bbaa0b3ec773061a142cba1
DawSula <63158940+DawSula@users.noreply.github.com>
2021-12-23 09:34:24 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 spl_autoload_register(function (string $classNamespace) {
-  $path = str_replace(['\\', 'App/'], ['/', ''], $classNamespace);
-  $path = "src/$path.php";
-  require_once($path);
+    $path = str_replace(['\\', 'App/'], ['/', ''], $classNamespace);
+    $path = "src/$path.php";
+    require_once($path);
 });
 
 require_once("src/Utils/debug.php");
@@ -21,25 +21,23 @@ use App\Controller\LogController;
 
 $request = new Request($_GET, $_POST, $_SERVER);
 session_start();
-//try {
-  if (!isset($_SESSION['start'])){
-      AbstractLogController::initConfiguration($configuration);
-      $logController = new LogController($request);
-      $logController->run();
+try {
+    if (!isset($_SESSION['start'])) {
+        AbstractLogController::initConfiguration($configuration);
+        $logController = new LogController($request);
+        $logController->run();
 
 
-  } else {
-      AbstractController::initConfiguration($configuration);
-      (new NoteController($request))->run();
-  }
-//  }
-//}catch (ConfigurationException $e) {
-//  //mail('xxx@xxx.com', 'Errro', $e->getMessage());
-//  echo '<h1>Wystąpił błąd w aplikacji</h1>';
-//  echo 'Problem z applikacją, proszę spróbować za chwilę.';
-// } catch (AppException $e) {
-//  echo '<h1>Wystąpił błąd w aplikacji</h1>';
-//  echo '<h3>' . $e->getMessage() . '</h3>';
-// } catch (\Throwable $e) {
-//  echo '<h1>Wystąpił błąd w aplikacji</h1>';
-// }
+    } else {
+        AbstractController::initConfiguration($configuration);
+        (new NoteController($request))->run();
+    }
+} catch (ConfigurationException $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo 'Problem z applikacją';
+} catch (AppException $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+    echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (\Throwable $e) {
+    echo '<h1>Wystąpił błąd w aplikacji</h1>';
+}
